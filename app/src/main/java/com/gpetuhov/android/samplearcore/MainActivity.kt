951fac9bee3d3a98170b74854d93685e98d5cf4d
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-04 13:36:16 +0300

    Check OpenGL version

--- app/src/main/java/com/gpetuhov/android/samplearcore/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcore/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.gpetuhov.android.samplearcore
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pawegio.kandroid.toast
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val MIN_OPENGL_VERSION = 3.0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         // In this sample we do not check if ARCore is supported,
         // because ARCore is declared as required in the manifest,
@@ -17,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         // because ArFragment automatically requests the camera permission
         // before creating the AR session.
 
+        // But we DO check, if the device has the required OpenGL version
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return
+        }
+
+        setContentView(R.layout.activity_main)
     }
 
     override fun onResume() {
@@ -25,5 +39,33 @@ class MainActivity : AppCompatActivity() {
         // We do not have to check whether ARCore is installed,
         // because ArFragment automatically checks
         // that ARCore is installed and up to date before creating the AR session.
+    }
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * Finishes the activity if Sceneform can not run
+     */
+    fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            toast("Sceneform requires Android N or later")
+            activity.finish()
+            return false
+        }
+
+        val openGlVersionString = (activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+            .deviceConfigurationInfo
+            .glEsVersion
+
+        if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
+            toast("Sceneform requires OpenGL ES 3.0 or later")
+            activity.finish()
+            return false
+        }
+
+        return true
     }
 }
