3070cb62d1b9563dfce9a83972c166c4bfa7857b
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-04 13:15:10 +0300

    Add comments

--- app/src/main/java/com/gpetuhov/android/samplearcore/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcore/MainActivity.kt
@@ -8,5 +8,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // In this sample we do not check if ARCore is supported,
+        // because ARCore is declared as required in the manifest,
+        // so the app will not be visible for devices, that do not support ARCore.
+
+        // We do not have to check Camera permission,
+        // because ArFragment automatically requests the camera permission
+        // before creating the AR session.
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // We do not have to check whether ARCore is installed,
+        // because ArFragment automatically checks
+        // that ARCore is installed and up to date before creating the AR session.
     }
 }
